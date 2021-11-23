dccf096c31a03a3b56fb31de6062278b7ddb181c
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-11-23 15:55:19 -0500

    remove context since it's not working

--- libintl_gettext/main.cpp
+++ libintl_gettext/main.cpp
@@ -3,7 +3,7 @@
 
 #define _(x)                    gettext(x)
 #define _n(singular, plural, n) ngettext(singular, plural, n)
-#define _d(context, msg)        dgettext(context, msg)
+//#define _d(context, msg)        cgettext(context, msg)
 #define N_(x)                   x
 
 constexpr auto saved_string = N_("Some global string\n");  // This will be translated later, so we already have to mark it with a macro to get it extracted.
@@ -17,15 +17,19 @@ int main()
   textdomain("main");
 
   fmt::print("{}\n", _("The first message."));
-  fmt::print(_n("We have {} message.", "We have {} messages.\n", 6), 6);
+  fmt::print(_n("We have {} message.", "We have {} messages.", 6), 6);
+  fmt::print("\n");
 
-  const auto open_file_label = _d("File", "open");  // with context
-  const auto open_door_label = _d("door", "open");  // with context
+//  const auto open_file_label = _d("file", "open");  // with context
+//  const auto open_door_label = _d("door", "open");  // with context
+
+//  fmt::print(open_file_label);
+//  fmt::print("\n");
+//  fmt::print("{}\n", open_door_label);
 
   fmt::print(_(saved_string));
-
-  fmt::print("{}\n", _("Aasdfasdf"));
-  fmt::print("{}\n", _("Casdfasd"));
-  fmt::print("{}\n", _("Basdfas"));
+  fmt::print("{}\n", _("Anything"));
+  fmt::print("{}\n", _("Clear"));
+  fmt::print("{}\n", _("Buffalo"));
   fmt::print("{}\n", _("The last message."));
 }
